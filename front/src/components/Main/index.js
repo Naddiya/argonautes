13f88dc89094592c589e968f1d47d89fb2512e2f
Naddiya <nadiamedkouri@gmail.com>
2021-03-11 02:08:03 +0100

    feat: dynamic crew members

--- front/src/components/Main/index.js
+++ front/src/components/Main/index.js
@@ -1,22 +1,42 @@
 import React from 'react';
+import axios from 'axios';
 
-const Main = () => {
+
+
+
+export default class Main extends React.Component {
+  state = {
+    members: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://localhost:3001/argos`)
+      .then(res => {
+        const members = res.data;
+        console.log(members)
+        this.setState({
+          members
+        });
+      })
+
+  }
+  render() {
     return (
-<main>
-    <h2>Ajouter un(e) Argonaute</h2>
-  <form className="new-member-form">
-    <label className="name">Nom de l&apos;Argonaute</label>
-    <input id="name" name="name" type="text" placeholder="Charalampos" />
-    <button type="submit">Envoyer</button>
-  </form>
-    <h2>Membres de l'équipage</h2>
-  <section className="member-list">
-    <div className="member-item">Eleftheria</div>
-    <div className="member-item">Gennadios</div>
-    <div className="member-item">Lysimachos</div>
-  </section>
-</main>
+
+      <main>
+      <h2> Ajouter un(e) Argonaute </h2> 
+        <form className = "new-member-form">
+          <label className = "name" > Nom de l' Argonaute </label> 
+          <input id = "name" name = "name" type = "text" placeholder = "Charalampos"/>
+          <button onClick > Envoyer </button> 
+        </form>
+      <h2> Membres de l 'équipage </h2> 
+        <section className = "member-list">
+          <div className = "member-item">{ this.state.members.map(member => <li key={member._id}>{member.name}</li>) } </div> 
+        </section>       
+      </main>
+
     )
+  }
 }
 
-export default Main;
