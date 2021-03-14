c4e9da9fd3ec9028c10ea7a9b18a896cf00acc0d
Naddiya <nadiamedkouri@gmail.com>
2021-03-14 10:36:27 +0100

    fix: correct membeSchema

--- front/backend/models/member.model.js
+++ front/backend/models/member.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const memberSchema = new Schema({
-  username: { type: String, required: true },
+  membername: { type: String, required: true },
 });
 
 const Exercise = mongoose.model('Member', memberSchema);
