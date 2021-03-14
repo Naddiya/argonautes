c2bd2acf2ea94cd1ac4cb6d8902e4d69757e26a0
Naddiya <nadiamedkouri@gmail.com>
2021-03-14 10:26:44 +0100

    refactor: remove back folder

--- back/models/Member.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const memberSchema = mongoose.Schema({
-  name: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Member', memberSchema);
